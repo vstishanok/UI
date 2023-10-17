6dc44c8fe5d9ffb80909289dc6323ce17dd5852e
Vadim Stishenok <v.stishenok@syberry.com>
2023-10-18 00:17:48 +0300

    test3

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <p>Hello World!</p>
-			<p>Tolday is cold!</p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
