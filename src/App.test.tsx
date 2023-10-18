aee7d5f27dd2ffd0f6bed024597486d54e2dd182
Vadim <148060081+vstishanok@users.noreply.github.com>
2023-10-19 01:56:29 +0300

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,11 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-            
   const linkElement = screen.getByText(/learn weather/i);
-
-
-
-  
   expect(linkElement).toBeInTheDocument();
 });
